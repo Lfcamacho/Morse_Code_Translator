04ba2873f099f891bf91b21a89a480248287c068
Luis Camacho <lfcamacho06@gmail.com>
2020-05-02 21:46:51 -0700

    Commented code

--- main_morsecode.py
+++ main_morsecode.py
@@ -8,68 +8,56 @@ import morse
 # Run method starts when you call thread1.start()
 
 class MyThread(QtCore.QThread):
-
     sig = pyqtSignal(list, str)
 
     def __init__(self, input_code):
-
         QtCore.QThread.__init__(self)
         self.input_code = input_code
-    
 
     def run(self):
-
-        try:
-            
+        try:   
             answer = morse.morsedecoder(self.input_code)
             self.sig.emit(answer, "")
-
         except:
-
             self.sig.emit([], "error")
 
-
+# Main GUI class
 class AppWindow(QMainWindow):
-
-    sig = pyqtSignal(list, str)
+    sig = pyqtSignal(list, str)     
 
     def __init__(self):
-
         super().__init__()
-
         self.ui = Ui_MainWindow()
         MainWindow = QtWidgets.QMainWindow()
-        self.ui.setupUi(self)
+        self.ui.setupUi(self)                   # Draw main widgets in GUI
         self.MainWindow = QMainWindow
         
-        self.ui.input_code.setFocus()
-        self.ui.translate_button.clicked.connect(self.clicked)
-        self.ui.input_code.returnPressed.connect(self.clicked)
+        self.ui.input_code.setFocus()                               
+        self.ui.translate_button.clicked.connect(self.clicked)      # start when translate button pressed
+        self.ui.input_code.returnPressed.connect(self.clicked)      # start when click enter
 
         self.show()
 
+    # Calculate all the answers 
     def clicked(self):
-
         self.ui.listWidget.clear()
         self.ui.statusbar.showMessage("Loading...")
         self.input_code = self.ui.input_code.text()
         self.thread1 = MyThread(self.input_code)
         self.thread1.sig.connect(self.PrintAnswers)
-        self.thread1.start()
+        self.thread1.start()                            # Start of Thread for morsedecoder
 
+    # Show answers in QListWidget
     def PrintAnswers(self, answer, error):
-
-        if error == "error":
-
+        # If an error is detected
+        if error == "error":        
             self.ui.statusbar.showMessage("This is not a valid input try using only . and -")
             self.msg = QtWidgets.QMessageBox()
             self.msg.setWindowTitle("Invalid Input")
             self.msg.setIcon(QtWidgets.QMessageBox.Critical)
             self.msg.setText("Invalid input\nThis is not a valid input. Try using only . and -")
             self.msg.exec_()
-        
         else:
-
             self.ui.statusbar.showMessage('Result: '+str(len(answer))+" options")
             self.ui.listWidget.addItems(answer)
 
--- morse.py
+++ morse.py
@@ -1,5 +1,6 @@
-def alphabet(letter):
 
+# function with a dictionary to decode form morse to text
+def alphabet(letter):
     morse = {
         ".-": "A",
         "-...": "B",
@@ -34,24 +35,20 @@ def alphabet(letter):
     else:
         return "no"
 
+# Main recursive function to search all possible answers
 def decoder(counter,phrase,code,words):
-
     global number
-    
     a = ""
     j = 1
     b = ""
 
     for j in range(1,6):
-
         if counter+j < len(code):
             b = b + (code[counter+j])
-            a = alphabet(b)
-
+            a = alphabet(b)             # conversion
             if a != "no":
                 phrase2 = phrase
                 phrase = phrase + a
-
                 if (counter+j) == (len(code)-1):
                     words.append(phrase)
                     number = number + 1
@@ -62,23 +59,12 @@ def decoder(counter,phrase,code,words):
 
     return words
 
+# sub function to initialize parameters
 def morsedecoder(code):
-
     global number
-
-    counter=-1
+    counter = -1
     number = 0
     phrase = ""
     words = []
-
     return decoder(counter,phrase,code,words)    
     
-
-
-#code = input("Insert morse code: ")
-#a = morsedecoder(".-.-.-.-.---.-.--.--.-.")
-
-#for x in a:
- #   print (x)
-
-#print("\nNumber of options: ",number)
